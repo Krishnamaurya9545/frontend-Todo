1b0ffefc0dc97ad183db6163e378536e6b3ca213
Krishnamaurya9545 <Krishnamaurya9545@gmail.com>
2024-08-02 18:23:52 +0530

    Resolved infinite loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState} from "react";
+import { useEffect, useState} from "react";
 import { CreateTodo  } from './components/CreateTodo'
 import {Todos } from './components/Todos'
 
 function App() {
   const [todos, setTodos] = useState([]);
 //Infinite request give use this fetch
-  // fetch("http://localhost:3000/todos")
-  //   .then(async function(res){
-  //      const json = await res.json();
-  //      setTodos(json.todos);
-  //   })
+//useEffect
+  useEffect(()=>{
+    fetch("http://localhost:3000/todos")
+      .then(async function(res){
+        const json = await res.json();
+        setTodos(json.todos);
+      })
+ },[])
+  
 
   return (
     <div>
